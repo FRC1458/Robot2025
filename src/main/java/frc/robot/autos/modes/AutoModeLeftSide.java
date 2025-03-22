c42c87d3c8e5171b60ec6f6d92a738a6075e87f5
Vivaan Patnaik <vivpat52@gmail.com>
2025-03-21 21:55:34 -0700

    rightside + comments

--- src/main/java/frc/robot/autos/modes/AutoModeLeftSide.java
+++ src/main/java/frc/robot/autos/modes/AutoModeLeftSide.java
@@ -6,23 +6,23 @@ public class AutoModeLeftSide extends AutoStringAuto {
               // Move from S3 to Reef E, 1s into the path Elevator will go to L4 (the path accounts for this and is slower near the end), then Shoot Coral
               "[ ( S 3 R E ) ( Wait 1 Elevator L4 ) ] CShoot " +   
 
-              // Elevator to  Ground, 0.2s after this has begun go to Coral Station (bot is stable even at L3, so 0.2s is enough)
+              // Elevator to  Ground, 0.1s after this has begun go to Coral Station (bot is stable even at L3, so 0.1s is enough)
               "[ ( Elevator Ground ) ( Wait 0.1 CS 2 ) ] " +
 
-              // Wait 0.1s at Coral Station to intake coral, then go to Reef D with the same parallel elevator logic as above, then Shoot Coral
-              "Wait 0.2 [ ( R D ) ( Wait 1 Elevator L4 ) ] CShoot " +
+              // Intake coral, then go to Reef D with the same parallel elevator logic as above, then Shoot Coral
+              "CIntake [ ( R D ) ( Wait 1 Elevator L4 ) ] CShoot " +
 
               // Elevator to Ground, go to Coral Station
               "[ ( Elevator Ground ) ( Wait 0.1 CS 2 ) ] " +
 
-              // Wait 0.1s at Coral Station, go to Reef C, Shoot Coral
-              "Wait 0.2 [ ( R C ) ( Wait 1 Elevator L4 ) ] CShoot " +
+              // Intake coral, go to Reef C, Shoot Coral
+              "CIntake [ ( R C ) ( Wait 1 Elevator L4 ) ] CShoot " +
 
               // Elevator to Ground, go to Coral Station
               "[ ( Elevator Ground ) ( Wait 0.1 CS 2 ) ] " +
 
-              // Wait 0.1s at Coral Station, go to Reef B, Shoot Coral
-              "Wait 0.2 [ ( R B ) ( Wait 2 Elevator L4 ) ] CShoot " +
+              // Intake coral, go to Reef B, Shoot Coral
+              "CIntake [ ( R B ) ( Wait 2 Elevator L4 ) ] CShoot " +
 
               // Elevator to Ground
               "Elevator Ground"
--- src/main/java/frc/robot/autos/modes/AutoModeRightSide.java
+++ src/main/java/frc/robot/autos/modes/AutoModeRightSide.java
@@ -6,23 +6,23 @@ public class AutoModeRightSide extends AutoStringAuto {
               // Move from S1 to Reef J, 1s into the path Elevator will go to L4 (the path accounts for this and is slower near the end), then Shoot Coral
               "[ ( S 1 R J ) ( Wait 1 Elevator L4 ) ] CShoot " +   
 
-              // Elevator to  Ground, 0.2s after this has begun go to Coral Station (bot is stable even at L3, so 0.2s is enough)
-              "[ ( Elevator Ground ) ( Wait 0.2 CS 1 ) ] " +              
+              // Elevator to  Ground, 0.1s after this has begun go to Coral Station (bot is stable even at L3, so 0.1s is enough)
+              "[ ( Elevator Ground ) ( Wait 0.1 CS 1 ) ] " +              
 
-              // Wait 0.1s at Coral Station to intake coral, then go to Reef K with the same parallel elevator logic as above, then Shoot Coral
-              "Wait 0.1 [ ( R K ) ( Wait 1.5 Elevator L4 ) ] CShoot " +
-
-              // Elevator to Ground, go to Coral Station
-              "[ ( Elevator Ground ) ( Wait 0.2 CS 1 ) ] " +          
-
-              // Wait 0.1s at Coral Station, go to Reef L, Shoot Coral
-              "Wait 0.1 [ ( R L ) ( Wait 1.5 Elevator L4 ) ] CShoot " +
+              // Intake coral, then go to Reef K with the same parallel elevator logic as above, then Shoot Coral
+              "CIntake [ ( R K ) ( Wait 1 Elevator L4 ) ] CShoot " +
 
               // Elevator to Ground, go to Coral Station
-              "[ ( Elevator Ground ) ( Wait 0.2 CS 1 ) ] " +      
+              "[ ( Elevator Ground ) ( Wait 0.1 CS 1 ) ] " +          
 
-              // Wait 0.1s at Coral Station, go to Reef A, Shoot Coral
-              "Wait 0.1 [ ( R A ) ( Wait 1.5 Elevator L4 ) ] CShoot " +
+              // Intake coral, go to Reef L, Shoot Coral
+              "CIntake [ ( R L ) ( Wait 1 Elevator L4 ) ] CShoot " +
+
+              // Elevator to Ground, go to Coral Station
+              "[ ( Elevator Ground ) ( Wait 0.1 CS 1 ) ] " +      
+
+              // Intake coral, go to Reef A, Shoot Coral
+              "CIntake [ ( R A ) ( Wait 2 Elevator L4 ) ] CShoot " +
 
               // Elevator to Ground
               "Elevator Ground"
